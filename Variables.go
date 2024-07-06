06c80ef07a072a0e421da73ca7f64875656b1d7c
André Saldanha <andre.g.saldanha@hotmail.com>
2024-07-05 22:11:35 -0300

    Initial commit

---
+++ Variables.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Definição de Variáveis")
+	//Definição de Variáveis
+	var number int = 10
+	var floatNumber float64 = 25.5
+	var text string = "André"
+	var booleano bool = false
+	const LIGHTSPEED uint32 = 299792458
+
+	//Exibindo os valores das variáveis
+	fmt.Println("Inteiro: ", number)
+	fmt.Println("Decimal: ", floatNumber)
+	fmt.Println("Texto: ", text)
+	fmt.Println("Booleano: ", booleano)
+	fmt.Println("Velocidade da Luz em km/s:", LIGHTSPEED)
+}
