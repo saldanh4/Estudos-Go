fee802ea7f4de5a6029b2c0256b1142cc6af3fef
André Saldanha <andre.g.saldanha@hotmail.com>
2024-07-07 20:26:49 -0300

    Initial commit

---
+++ Loops.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//Laço for para exibir os inteiros de à 9
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	//Laço for para exibir a letra y infinitamente
+	for i := 1; i == 0; i++ {
+		fmt.Print("y")
+	}
+}
