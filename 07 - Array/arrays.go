bea47f0e4b2b23ab5212a9fa8f7a5f2817c2a258
André Saldanha <andre.g.saldanha@hotmail.com>
2024-07-10 21:16:48 -0300

    Improving array study

--- 07 - Array/arrays.go
+++ 07 - Array/arrays.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var myArray [2]int
+	//Criando array com valores inicializados
+	var myArray = [2]int{2, 3}
 
+	//exibindo array
+	fmt.Println(myArray)
+
+	//Atribuindo novos valores
 	myArray[0] = 10
 	myArray[1] = 20
 
-	fmt.Println(myArray)
+	//exibindo um elemento do array
+	fmt.Println(myArray[0])
+
 	//aqui retorna erro Index 2 out of bounds por estar fora do tamanho do array
 	//myArray[2] = 30
 
+	//exibindo a posição e respectivo valor no array usando o for e range
 	for i, j := range myArray {
 		fmt.Printf("Posição %d = %v\n", i, j)
 	}
