9e0fad160b435408ea82c4c33e1770d5f66c0381
André Saldanha <andre.g.saldanha@hotmail.com>
2024-07-20 20:49:20 -0300

    Interfaces

--- 04-Loops/Loops.go
+++ 04-Loops/Loops.go
@@ -14,4 +14,10 @@ func main() {
 	for i := 1; i == 0; i++ {
 		fmt.Print("y")
 	}
+
+	//Laço sem parâmetros. Legal que isso permite o uso do For como o While, declarando apenas a condicional no loop e a variável de controle fora.
+	//Achei isso perigoso em uma aplicação, caso alguém crie o laço para uma iteração futura e esqueça de complementar ou deixar comentado
+	for {
+		fmt.Print("y")
+	}
 }
